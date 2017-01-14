77a1122178fe0109814a1052afd26cfec2aecbf0
Benjamin Matson <benjaminmatson@gmail.com>
2017-01-14 16:02:41 -0700

    correct seed complete, track includes artist_id

---
+++ migrations/20170114153551_track.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("track", function (table) {
+    table.increments()
+    table.text("name")
+    table.integer("artist_id")
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("track")
+}
